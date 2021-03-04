25afbe6c7081c24f0f302ee5cc5c48de4b1a86ca
paullally <paullally98@gmail.com>
2021-03-04 15:13:41 +0000

    Completed form function to post data to google sheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
- const scriptURL = 'https://script.google.com/macros/s/AKfycbyP-td9x4t92On26bY97-Pqibj3QYm1CUIGefchYVJwLkdAgGmZYHB0enygbnwWD0WW/exec'
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbwuW-Yuu3qIEhnvYOfffH5Fa31ATOntwSbEhjCbIAm_KP6symTMCnqGg6RZkbcM0xnx6A/exec'
             const form = document.forms['google-sheet']
             form.addEventListener('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .then(response => setTimeout(function(){ document.getElementById('thanks').classList.remove("after-submit"); document.getElementById('myForm').classList.add("after-submit");document.getElementById('contact-us').classList.add("after-submit")}, 1))
                 .catch(error => console.error('Error!', error.message))
-            })
+            });
 
