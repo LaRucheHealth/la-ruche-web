2e74ac39a7dc3fa87c02cdac7db92ed22a8cda8d
paullally <paullally98@gmail.com>
2021-03-11 17:53:23 +0000

    Edited submission form link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
- const scriptURL = 'https://script.google.com/macros/s/AKfycbws2HFwMZJLnZ99rReIUVy8IyRHpTeKuP2X9GSlwzmLqpgh8LxcdiP2rDPKY-F1hZ-R/exec'
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbyyOxfKle-g-TADbK6Sn24Ka07H0D8dcWUOGjGa2FAkeY6iMiq1kfj8YU4QZefuW30U3Q/exec'
             const form = document.forms['google-sheet']
             form.addEventListener('submit', e => {
               e.preventDefault()
@@ -6,4 +6,3 @@
                 .then(response => setTimeout(function(){ document.getElementById('thanks').classList.remove("after-submit"); document.getElementById('myForm').classList.add("after-submit")}, 1))
                 .catch(error => console.error('Error!', error.message))
             });
-
