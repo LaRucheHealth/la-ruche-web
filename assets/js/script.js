4b0d698a6682c9de7e97d8a98888fde0d35cdc22
paullally <paullally98@gmail.com>
2021-03-12 11:41:31 +0000

    Addded modals and functionality to pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
- const scriptURL = 'https://script.google.com/macros/s/AKfycbyyOxfKle-g-TADbK6Sn24Ka07H0D8dcWUOGjGa2FAkeY6iMiq1kfj8YU4QZefuW30U3Q/exec'
-            const form = document.forms['google-sheet']
-            form.addEventListener('submit', e => {
-              e.preventDefault()
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => setTimeout(function(){ document.getElementById('thanks').classList.remove("after-submit"); document.getElementById('myForm').classList.add("after-submit")}, 1))
-                .catch(error => console.error('Error!', error.message))
-            });
+
+        
+var modal = document.getElementById("myModal");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+  modal.style.display = "none";
+}
