8444d351e8bc4188ec0d7bc629625b454f34a077
Benjamin Yeboah Sasu <sasuyeboahbenjamin@gmail.com>
2022-01-17 13:50:42 +0000

    added custom script to auto stop video when modal is closed

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    // Gets the video src from the data-src on each button
+    var $videoSrc;  
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data( "src" );
+    });
+    console.log($videoSrc);
+    console.log("Printed when document is ready")
+      
+      
+    // when the modal is opened autoplay it  
+    $('#myModal').on('shown.bs.modal', function (e) {
+        
+    // set the video src to autoplay and not to show related video. 
+    $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+    })
+      
+    
+    
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        this.querySelector('video').pause();
+        $("#video").attr('src',$videoSrc); 
+    }) 
+        
+        
+    
+    
+      
+      
+    // document ready  
+    });
