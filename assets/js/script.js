dd733e1921ee88a304cdd8946b9f70f1e91c0593
paullally <paullally98@gmail.com>
2022-01-29 15:45:54 +0000

    update contact from after submit issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,6 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbws2HFwMZJLnZ99rReIUV
             form.addEventListener('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => setTimeout(function(){ document.getElementById('thanks').classList.remove("after-submit"); document.getElementById('myForm').classList.add("after-submit")}, 1))
+                .then(response => setTimeout(function(){ document.getElementById('thanks').classList.remove("after-submit"); document.getElementById('form').classList.add("after-submit")}, 1))
                 .catch(error => console.error('Error!', error.message))
             });
